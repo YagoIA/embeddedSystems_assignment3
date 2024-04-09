fe29837635bfdd49233be697384143e51cba9590
yago <yago.illing@googlemail.com>
2024-04-09 19:42:14 +0200

    backend logic

--- river-monitoring-service/SystemVariables.js
+++ river-monitoring-service/SystemVariables.js
@@ -1,5 +1,26 @@
 export class SystemVariables{
+	States = {
+		normal: "normal",
+		too_low: "too-low",
+		pre_too_high: "pre-too-high",
+		too_high: "too-high",
+		too_high_criticial: "too-high-critical"
+	}
+
+	MonitoringFrequencies = {
+		alarmMonitoringFrequency: 500,
+		normalMonitoringFrequency: 1000
+	}
+
 	constructor(valveOpeningLevel) {
 		this.valveOpeningLevel = valveOpeningLevel;
+
+
+		this.monitoringFrequencyMs = 1000 
+		this.state = this.States.normal
+
+		//placeholder values
+		this.waterLevel = 100
+		this.waterLevelRange = [50, 200, 300, 400]
 	  }
 }
--- river-monitoring-service/app.js
+++ river-monitoring-service/app.js
@@ -49,53 +49,115 @@ client.on("connect", () => {
 
 client.on("message", (topic, payload) => {
   console.log("Received Message:", topic, payload.toString());
+
+  //TODO on ESP32 side
+  if(topic == "water-level"){
+    //sysVars.waterLevel = payload.waterLevel
+  }
+
+
 });
 
-const port = new SerialPort({
-  path: commPort,
-  baudRate:9600
-});
+process.on('uncaughtException', function (err) {
+  if(!err.message.includes("No such file or directory, cannot open")){
+    this.emit('error', err)
+  }
+})
 
-const parser = port.pipe(new ReadlineParser());
+try {
 
-
-port.on("open", () => {
-  console.log('serial port open');
-});
-
-parser.on('data', data =>{
-  console.log('Arduino: ', data);
-});
-
-
-setInterval(() => {
-  port.write(sysVars.valveOpeningLevel + '\n', (err) => {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-    console.log('message written');
+  var port = new SerialPort({
+    path: commPort,
+    baudRate:9600,
   });
+  
+  const parser = port.pipe(new ReadlineParser());
+  
+  
+  port.on("open", () => {
+    console.log('serial port open');
+  });
+  
+  parser.on('data', data =>{
+    console.log('Arduino: ', data);
+  });
+  
+} catch(error) {  
+  const errorMessage = util.inspect(error); 
+  console.error(errorMessage); 
+}
 
-}, 1000);
+
+
+
+var loopFunction = function(){
+  console.log(sysVars.state)
+  if(sysVars.waterLevel < sysVars.waterLevelRange[0]) {
+    sysVars.monitoringFrequencyMs = sysVars.MonitoringFrequencies.normalMonitoringFrequency
+    sysVars.state = sysVars.States.too_low
+    setValve(0)
+  }
+
+  if(sysVars.waterLevel >= sysVars.waterLevelRange[0] && 
+    sysVars.waterLevel <= sysVars.waterLevelRange[1]) {
+    sysVars.state = sysVars.States.normal
+    sysVars.monitoringFrequencyMs = sysVars.MonitoringFrequencies.normalMonitoringFrequency
+    setValve(25)
+  }
+
+  if(sysVars.waterLevel > sysVars.waterLevelRange[1]) {
+    sysVars.monitoringFrequencyMs = sysVars.MonitoringFrequencies.alarmMonitoringFrequency
+    if(sysVars.waterLevel >= sysVars.waterLevelRange[2] &&
+       sysVars.waterLevel < sysVars.waterLevelRange[3]){
+        sysVars.state = sysVars.States.too_high
+        setValve(50)
+    }
+
+    if(sysVars.waterLevel > sysVars.waterLevelRange[3]){
+      sysVars.state = sysVars.States.too_high_criticial
+      setValve(100)
+    }
+    
+  }
+  setTimeout(loopFunction, sysVars.monitoringFrequencyMs);
+}
+setTimeout(loopFunction, sysVars.monitoringFrequencyMs);
+
+function setValve(valveOpeningLevel){
+  if(port != null && port.isOpen){
+    port.write(sysVars.valveOpeningLevel + '\n', (err) => {
+      if (err) {
+        return console.log('Error on write: ', err.message);
+      }
+      sysVars.valveOpeningLevel = valveOpeningLevel
+      console.log('valve opening level set');
+    });
+  } else{
+    console.log('could not set valve opening level')
+  }
+
+}
 
 
 import express from 'express';
-
 const expressPort = 3001
-
 const app = express();
-
 app.use(express.json())
-
 app.post('/valveOpeningLevel', (req, res) => {
   console.log(req.body.value);
+
+  //DEBUG
+  //sysVars.waterLevel = req.body.value;
+  
   if(req.body.value >= 0 && req.body.value <= 100){
     sysVars.valveOpeningLevel = req.body.value;
+    
+    
   }
   
   res.send('POST request to the homepage')  
 })
 app.listen(expressPort, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${expressPort}`)
 })
 
